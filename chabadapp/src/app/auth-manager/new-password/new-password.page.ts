e2453ba422fa44097ef1dc76b451dbd4a53068c5
phuoctai12t <phuoctai12t@gmail.com>
2021-01-07 15:24:08 +0700

    new password

--- chabadapp/src/app/auth-manager/new-password/new-password.page.ts
+++ chabadapp/src/app/auth-manager/new-password/new-password.page.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-password.page.scss'],
 })
 export class NewPasswordPage implements OnInit {
+  passwordValue = '444';
+  confirmedPasswordValue = '';
+
+  invalidPassword = '';
+  invalidConfirmedPassword = '';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  clearPassword(event) {
+    event.target.value = '';
+    this.invalidPassword = '';
+    this.invalidConfirmedPassword = '';
   }
 
+  saveValue($event) {
+    if (event.target.name == 'password') {
+      this.passwordValue = event.target.value;
+    } else if (event.target.name == 'confirmedPassword') {
+      this.confirmedPasswordValue = event.target.value;
+    }
+  }
+
+  checkValidPassword(name: string, value: string) {
+    if (value == '') {
+      return `${name} can't not be empty`;
+    }
+    if (value.length < 8) {
+      return `${name} can't not be less than 8 letters`;
+    }
+    if (name == 'Confirmed password') {
+      if (this.passwordValue != this.confirmedPasswordValue) {
+        return 'Confirmed password not match with password';
+      }
+    }
+    return '';
+  }
+
+  confirmPassword() {
+    this.invalidPassword = this.checkValidPassword('Password', this.passwordValue);
+    this.invalidConfirmedPassword = this.checkValidPassword('Confirmed password', this.confirmedPasswordValue);
+    if (this.invalidPassword == '' && this.invalidConfirmedPassword == '') {
+      
+    }
+  }
 }

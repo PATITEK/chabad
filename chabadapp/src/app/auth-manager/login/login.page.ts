d67cee8fe9dbf4538949b9b13ac259ee048c0c74
hcmutemimi <hoaimiqng@gmail.com>
2021-01-07 09:00:48 +0700

    fix bug flag

--- chabadapp/src/app/auth-manager/login/login.page.ts
+++ chabadapp/src/app/auth-manager/login/login.page.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  public type = 'password';
+  public showpass = false;
+  public name = 'eye-outline';
+  public status='login';
   constructor() { }
-
   ngOnInit() {
+  }
+  showPass(){
+    this.showpass = !this.showpass;
+    if (this.showpass) {
+      this.type = 'text';
+      this.name = 'eye-off-outline'
+    }
+    else {
+      this.type = 'password';
+      this.name = 'eye-outline'
+    }
+  }
+  clickBtnLogin(){
+    this.status="login";
+  }
+  clickBtnSign(){
+    this.status="sign";
   }
 
 }

d0c0b8351ec915201dc984b2fcbc028242abd14c
kawasaky007 <54396843+kawasaky007@users.noreply.github.com>
2021-01-18 16:09:00 +0700

    fix (#34)

--- chabadapp/src/app/main/shopping/shopping.page.ts
+++ chabadapp/src/app/main/shopping/shopping.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shopping',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.page.scss'],
 })
 export class ShoppingPage implements OnInit {
-
-  constructor() { }
+  temples = [
+    {
+      id: 1,
+      imgUrl: 'assets/img/temple.jpg',
+      distance: '2,4',
+      name: 'Chabad of Ho Chi Minh City',
+      address: '5a (villa) Nguyen Dinh Chieu, Phuong Dakao, District 1 Thành phố Hồ Chí Minh',
+    },
+    {
+      id: 2,
+      imgUrl: 'assets/img/temple.jpg',
+      distance: '2,4',
+      name: 'Chabad of Ho Chi Minh City',
+      address: '5a (villa) Nguyen Dinh Chieu, Phuong Dakao, District 1 Thành phố Hồ Chí Minh',
+    }
+  ]
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,5 +38,20 @@ export class ShoppingPage implements OnInit {
     Object.keys(tabs1).map((key) => {
       tabs1[key].style.display = 'block';
     });
+  }
+
+  goToNavBarItem(navBarItem) {
+    this.router.navigateByUrl(`main/${navBarItem.name}`);
+  }
+
+  goToTemple(temple) {
+    const data = {
+      id: temple.id
+    }
+    this.router.navigate(['/main/synagogue/temple'], {
+      queryParams: {
+        data: JSON.stringify(data)
+      }
+    })
   }
 }
